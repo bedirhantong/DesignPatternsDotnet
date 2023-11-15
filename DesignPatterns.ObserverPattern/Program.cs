b149494af579a85e1018c05aa4cf03d0b40b011e
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-11-15 23:42:48 +0300

    Observer pattern example is added

--- DesignPatterns.ObserverPattern/Program.cs
+++ DesignPatterns.ObserverPattern/Program.cs
@@ -1,11 +1,15 @@
 ﻿// Behavioral Design Pattern Category : Observer pattern //
 
 var xiaomi = new Product("Xiaomi Redmi 10", 1000);
+var iphone = new Product("Iphone XS max Pro", 1000);
 var amazon = new Amazon();
 var bedirhanObserver = new BedirhanObserver("Bedirhan Tong");
+var serhanObserver = new SerhanObserver("Serhan Baymaz");
 
 amazon.Register(bedirhanObserver, xiaomi);
-amazon.NotifyByProductName("Xiaomi Redmi 10");
+amazon.Register(serhanObserver, iphone);
+//amazon.NotifyByProductName("Xiaomi Redmi 10");
+amazon.NotifyAll();
 
 Console.ReadLine();
 
@@ -43,13 +47,27 @@ class Amazon
 
 interface IObserver
 {
+    string Name { get; set; }
+
     void StockUpdate(Product product);
+}
+
+class SerhanObserver : IObserver
+{
+    public string Name { get; set; }
+    public SerhanObserver(String Name) { this.Name = Name; }
+    public SerhanObserver() { }
+
+    public void StockUpdate(Product product)
+    {
+        Console.WriteLine("Sayın " + Name + " " + product.Name + " ürünü stoklarımıza gelmiştir.");
+    }
 }
 
 class BedirhanObserver : IObserver
 {
-    private String Name {  get; set; }
-    public BedirhanObserver(String Name) {  this.Name = Name; }
+    public string Name { get; set; }
+    public BedirhanObserver(String Name) { this.Name = Name; }
     public BedirhanObserver() { }
 
     public void StockUpdate(Product product)
