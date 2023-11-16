defb24365928f2ca6aed7a60e6ae167ddc0a94d2
Bedirhan Tong <bedirhantongdev@gmail.com>
2023-11-16 16:45:43 +0300

    Factory design pattern :  footbal tournaments example is added

---
+++ DesignPatterns.FactoryPattern.ConsoleGame/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// PlayStationFactory
+IGameFactory psFactory = new PlaystationFactory();
+IGame ps = psFactory.CreatePlatform();
+ps.PlatformSpecific();
+
+// PcFactory
+IGameFactory pcFactory = new PcFactory();
+IGame pc = pcFactory.CreatePlatform();
+pc.PlatformSpecific();
+
+
+// AtariFactory
+IGameFactory atariFactory = new AtariFactory();
+IGame atari = atariFactory.CreatePlatform();
+atari.PlatformSpecific();
+
+Console.ReadLine();
+
+interface IGame
+{
+    void PlatformSpecific();
+}
+
+class Atari : IGame
+{
+    public void PlatformSpecific()
+    {
+        Console.WriteLine("Bu oyun Atari platformunda geçerli bir oyundur.");
+    }
+}
+
+class Pc : IGame
+{
+    public void PlatformSpecific()
+    {
+        Console.WriteLine("Bu oyun Pc platformunda geçerli bir oyundur.");
+    }
+}
+
+
+class Playstation : IGame
+{
+    public void PlatformSpecific()
+    {
+        Console.WriteLine("Bu oyun Playstation platformunda geçerli bir oyundur.");
+    }
+}
+
+
+interface IGameFactory
+{
+    IGame CreatePlatform();
+}
+
+class PcFactory : IGameFactory
+{
+    public IGame CreatePlatform()
+    {
+        return new Pc();
+    }
+}
+
+class PlaystationFactory : IGameFactory
+{
+    public IGame CreatePlatform()
+    {
+        return new Playstation();
+    }
+}
+
+class AtariFactory : IGameFactory
+{
+    public IGame CreatePlatform()
+    {
+        return new Atari();
+    }
+}
---
+++ DesignPatterns.FactoryPattern.FifaWorldCup/Program.cs
@@ -0,0 +1,94 @@
+﻿
+
+Uefa istanbulTournament = new IstanbulTournament();
+ITournament tournament = istanbulTournament.StartATournament("WorldCup");
+ITournament tournament3 = istanbulTournament.StartATournament("ChampionsLeague");
+
+Uefa zurihTournament = new ZurihTournament();
+//ITournament tournament1 = zurihTournament.StartATournament("WorldCup");
+ITournament tournament4 = zurihTournament.StartATournament("ChampionsLeague");
+
+Console.ReadLine();
+
+interface ITournament
+{
+    void Start();
+    void PlayTournamentMusic();
+    void Finish();
+}
+
+class WorldCup : ITournament
+{
+    public void Finish()
+    {
+        Console.WriteLine("WorldCup is finished");
+    }
+
+    public void PlayTournamentMusic()
+    {
+        Console.WriteLine("WorldCup's tournament music is playing.");
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("WorldCup is started");
+    }
+}
+
+class ChampionsLeague : ITournament
+{
+    public void Finish()
+    {
+        Console.WriteLine("ChampionsLeague is finished");
+    }
+
+    public void PlayTournamentMusic()
+    {
+        Console.WriteLine("ChampionsLeague's tournament music is playing.");
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("ChampionsLeague is started");
+    }
+}
+
+abstract class Uefa
+{
+    protected abstract ITournament CreateATournament(string type);
+
+    public ITournament StartATournament(string type) {
+        ITournament tournament = CreateATournament(type);
+        tournament.Start();
+        tournament.PlayTournamentMusic();
+        tournament.Finish();
+
+        return tournament;
+    }
+}
+
+class IstanbulTournament : Uefa
+{
+    protected override ITournament CreateATournament(string type)
+    {
+        return type switch
+        {
+            "WorldCup" => new WorldCup(),
+            "ChampionsLeague" => new ChampionsLeague(),
+             _ => throw new Exception($"There is no {nameof(type)} tournament."),
+        };
+    }
+}
+
+
+class ZurihTournament : Uefa
+{
+    protected override ITournament CreateATournament(string type)
+    {
+        return type switch
+        {
+            "ChampionsLeague" => new ChampionsLeague(),
+            _ => throw new ArgumentException($"There is no {nameof(type)} tournament."),
+        };
+    }
+}
